738f0ea96534d158dc918225fed343c7d873a205
Tanjil Hassan <kenk14382@gmail.com>
2021-10-24 18:00:08 +0600

    added ema-jhon private router website

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD-gPvtzMOiTedU6_D9MWeQm9ltVBNAwE4",
+    authDomain: "ema-jhon-b6a7b.firebaseapp.com",
+    projectId: "ema-jhon-b6a7b",
+    storageBucket: "ema-jhon-b6a7b.appspot.com",
+    messagingSenderId: "31736545538",
+    appId: "1:31736545538:web:65afb1da6896aae9b1821b"
+};
+
+export default firebaseConfig
